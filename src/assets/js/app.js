755dcc53d47ebd6a2ee480353fe77dc0a66bcec3
rath-prak <rathanap@gmail.com>
2017-11-14 02:33:39 +1300

    add greensock

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import whatInput from 'what-input';
+import {TweenMax} from "gsap";
 
 window.$ = $;
 
@@ -7,6 +8,9 @@ import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
+
+var logo = document.getElementsByClassName('logo');
+TweenMax.to(logo, 1, {x:500});
 
 
 $(document).foundation();
