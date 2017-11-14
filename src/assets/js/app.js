759d906a00547871796da325306c3210951ed03c
rath-prak <rathanap@gmail.com>
2017-11-14 23:28:03 +1300

    add content sections

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -11,8 +11,24 @@ import Foundation from 'foundation-sites';
 
 // button animatio for main nav 'PORTFOLIO Button'
 btnAnimation();
+// overlay navigation
+var overlayNav = document.querySelector('.overlay-nav');
+var portfolioBtn = document.querySelector('.portfolio-btn');
+var closeMenu = document.querySelector('#close-menu');
 
+portfolioBtn.addEventListener('click', () => {
+  TweenMax.to(overlayNav, 1, {
+    y: 0,
+    ease: Power3.easeOut,
+  })
+})
 
+closeMenu.addEventListener('click', () => {
+  TweenMax.to(overlayNav, 1, {
+    y: '-100%',
+    ease: Power3.easeOut,
+  })
+})
 
 
 $(document).foundation();
