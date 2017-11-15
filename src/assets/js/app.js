afff733694e041dd4e588f421ca36056fdec331e
rath-prak <rathanap@gmail.com>
2017-11-16 11:39:17 +1300

    work on navigation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -16,18 +16,16 @@ var overlayNav = document.querySelector('.overlay-nav');
 var portfolioBtn = document.querySelector('.portfolio-btn');
 var closeMenu = document.querySelector('#close-menu');
 
+tl = new TimelineMax({ paused:true, reversed:true })
+tl.to(overlayNav, 1, { x: '100%', ease: Power2.easeOut })
+// tl.add(TweenMax.to(box2, 1, {x: 400, opacity: 1, delay: 2}));
+
 portfolioBtn.addEventListener('click', () => {
-  TweenMax.to(overlayNav, 1, {
-    y: 0,
-    ease: Power3.easeOut,
-  })
+  tl.reversed() ? tl.restart() : tl.reverse();
 })
 
 closeMenu.addEventListener('click', () => {
-  TweenMax.to(overlayNav, 1, {
-    y: '-100%',
-    ease: Power3.easeOut,
-  })
+  tl.reversed() ? tl.restart() : tl.reverse();
 })
 
 
